2adda2cf1aa32f31e59d8076163a2a5b7edade94
Carlos Chavez <charlyccr4@gmail.com>
2025-09-25 18:26:21 -0600

    Version 1.1 agregada la funcion raizCuadrada y corroborado su correcto funcionamiento 18:18 25/09/2025

--- clases/avanzadas.py
+++ clases/avanzadas.py
@@ -4,13 +4,15 @@ class Avanzadas:
     def __init__(self):
         self.base = 0
         self.exponente = 0
+        
+    def leerNumeros(self):
+        self.base = float(input("Ingresa la base: "))
+        self.exponente = float(input("Ingresa el exponente: "))
 
     def elevarPotencia(self):
         return math.pow(self.base, self.exponente)
     
     def raizCuadrada(self):
-        return math.sqrt(self.num1)
-
-    def leerNumeros(self):
-        self.base = float(input("Ingresa la base: "))
-        self.exponente = float(input("Ingresa el exponente: "))
+        if self.base < 0:
+            return "Error: No se puede calcular la raíz cuadrada de un número negativo."
+        return math.sqrt(self.base)
